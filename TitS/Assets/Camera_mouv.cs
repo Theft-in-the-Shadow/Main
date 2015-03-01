e40f2d15191e839041120742cbc63f5ab8ebfe42
Adrien <adrien.943@gmail.com>
2015-03-01 18:33:54 +0100

    1 IA avec parcours, perso princ. de test, camera qui tourne

---
+++ TitS/Assets/Camera_mouv.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_mouv : MonoBehaviour {
+
+	public float angle = 90f;
+	public float fieldOfViewAngle = 30f;
+
+	private GameObject light ;
+	private GameObject rotat;
+
+	void Awake () 
+	{
+		light = GameObject.Find ("Directional light");
+		rotat = GameObject.Find ("CamArmature");
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotat.transform.Rotate (0, 0, -1);
+		light.transform.Rotate (0, -1, 0);
+	
+	}
+}
