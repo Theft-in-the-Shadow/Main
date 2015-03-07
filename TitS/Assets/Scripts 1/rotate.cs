4d125ae2b55c2c9bd528bcd2e093bf048a8b87cf
Adrien <adrien.943@gmail.com>
2015-03-07 14:32:31 +0100

    ajout du perso par bertrait

---
+++ TitS/Assets/Scripts 1/rotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Mouse X")!=0) 
+		{
+			transform.RotateAround (GameObject.Find("GameObject").transform.position,Vector3.up,Input.GetAxis ("Mouse X")*1000*Time.deltaTime);
+		}
+
+	}
+}
