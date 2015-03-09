f724000f30899107639c36d2377ac95446b1b34d
Maxime <maxime.chouquet@orange.fr>
2015-03-09 17:23:37 +0100

    fin

---
+++ TitS/Assets/TEMPS.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TEMPS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Horizontal") != 0) {
+						Time.timeScale = 0;	
+				}
+
+		}
+
+}
